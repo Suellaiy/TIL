afbe31667ab52b1f1c49658551f001eefae3619a
Suellaiy <dudrbs117@naver.com>
2019-11-06 17:03:47 +0900

    If I is 15multipe, say 'pizzbuzz'

--- python/191106-fizzbuzz2.py
+++ python/191106-fizzbuzz2.py
@@ -1,6 +1,8 @@
 for i in range (1, 100+1):
 
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print("pizzbuzz")
+    elif i % 3 == 0:
         print("pizz")
     elif i % 5 == 0:
         print("buzz")
