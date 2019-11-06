16040a65ec6178626c35226b8f1f01074d7a7c9b
Suellaiy <dudrbs117@naver.com>
2019-11-06 17:20:45 +0900

    feat: make list 1 to 100

--- python/191106-fizzbuzz2.py
+++ python/191106-fizzbuzz2.py
@@ -1,6 +1,1 @@
-for i in range (1, 100+1):
-
-    if ( i % 3 == 0 or i % 5 == 0):
-        print(("puzz")* (i%3==0) + ("buzz")* (i%5==0))
-    else:
-        print(i)
+print([ i for i in range (1, 1+100)])
